ca57125239815569ed2edd91ccdc2c955d69c52a
helen2003 <106905099+helen2003@users.noreply.github.com>
2025-02-19 08:24:01 +0500

    change 7

--- src/answer/dto/create-update-answer.input.ts
+++ src/answer/dto/create-update-answer.input.ts
@@ -4,10 +4,14 @@ import { AnswerModel } from '../model/answer.model';
 @InputType()
 export class CreateUpdateAnswerInput extends PickType(AnswerModel, [
   'text',
+  'correct',
   'questionId',
 ]) {
   @Field(() => String)
   text: string;
+
+  @Field(() => String)
+  correct: string;
 
   @Field(() => Int)
   questionId: number;
